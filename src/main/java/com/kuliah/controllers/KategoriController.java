0c24e9c2173d46900507aa1444eb7e39b94a1371
Tio Haidar <atiohaidar@gmail.com>
2023-04-26 16:45:15 +0700

    # bikin rest server belom fix ## kurang apa aja? - nge benerin format json nya - nge bikin yang todo (karena butuh relasi)
    - bikin rest client nya

--- src/main/java/com/kuliah/controllers/KategoriController.java
+++ src/main/java/com/kuliah/controllers/KategoriController.java
@@ -2,59 +2,60 @@ package com.kuliah.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kuliah.models.entities.KategoriEntity;
 import com.kuliah.services.KategoriService;
-
-@Controller
+@RestController
 @RequestMapping("")
 public class KategoriController {
     @Autowired
     KategoriService kategoriService;
     @GetMapping
-    public String showAllKategori(){
+    public Iterable<KategoriEntity> showAllKategori(){
         Iterable<KategoriEntity> all_kategori = kategoriService.findAll();
         
-return "/Kategori/lihatKategori";
+return all_kategori;
     }
 
     @GetMapping("/edit/{id}")
-    public String editKategori(@PathVariable("id") Integer id){
+    public KategoriEntity editKategori(@PathVariable("id") Integer id){
         KategoriEntity kategori = kategoriService.findOne(id);
-        return "/Kategori/editKategori";
+        return kategori;
     }
     @PostMapping("/edit/{index}")
     public String editKategoriPost(@RequestBody KategoriEntity kategori){
         kategoriService.save(kategori);
 
-        return "/Kategori/editKategori";
+        return "berhasil";
     }
     @GetMapping("/delete/{id}")
-    public String deleteKategori(@PathVariable("id") Integer id){
+    public KategoriEntity deleteKategori(@PathVariable("id") Integer id){
         KategoriEntity kategori = kategoriService.findOne(id);
 
-        return "/Kategori/hapusKategori";
+        return kategori;
     }
-    @PostMapping("/delete/{id}")
-    public String deleteKategoriPost(@RequestBody boolean hapus, @PathVariable("id") Integer id){
-            if (hapus){
-                kategoriService.removeBy(null);
-            }
-        return "/Kategori/hapusKategori";
+    @DeleteMapping("/delete/{id}")
+    public String deleteKategoriPost(/*@RequestBody boolean hapus,*/ @PathVariable("id") Integer id){
+            //if (hapus){
+                kategoriService.removeBy(id);
+            //}
+        return "berhasil";
     }
     @GetMapping("/add")
     public String createKategoriGet(){
-        return "/Kategori/tambahKategori";
+        return "silahkan";
     }
     @PostMapping("/add")
     public String createKategoriPost(@RequestBody KategoriEntity new_kategori){
         kategoriService.save(new_kategori);
-        return "/Kategori/tambahKategori";
+        return "berhasil";
     }
 
 
